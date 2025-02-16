92fc5f20e866919c86aeb71c7346cc9d06b759f8
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-02-16 12:19:01 +0530

    Created structure for explore menu list

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -4,8 +4,19 @@ import { menu_list } from '../../assets/assets'
 
 const ExploreMenu = () => {
   return (
-    <div>
-
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingridents and culinary experties. Our mission is to satisfy your cravings and elevate your dinning experience, one delicious meal at a time.</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+          return (
+            <div key={index} className='explore-menu-list-item'>
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
