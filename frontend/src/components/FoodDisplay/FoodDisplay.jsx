c177a11b68e166ae028096329ab7ecdb85a52569
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-02-16 15:50:59 +0530

    Importing StoreContext for Food Display

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodDisplay.css'
+import { StoreContext } from '../../Context/StoreContext'
 
 const FoodDisplay = () => {
+
+    const {food_list} = useContext(StoreContext)
+
   return (
     <div>
 
