86b7e4a31f43e5a1f6e3da39154bb1c83a4abea1
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-02-22 12:13:46 +0530

    Added CSS for Login Sign up Page

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -14,9 +14,9 @@ const LoginPopup = ({ setShowLogin }) => {
           <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className="login-popup-inputs">
-          {currState === "Login" ? <></> : <input type="text" placeholder='your name' required />}
-          <input type="email" placeholder='your email' required />
-          <input type="password" placeholder='password' required />
+          {currState === "Login" ? <></> : <input type="text" placeholder='Your name' required />}
+          <input type="email" placeholder='Your email' required />
+          <input type="password" placeholder='Password' required />
         </div>
         <button>{currState === "Sign Up" ? "Create Account" : "Login"}</button>
         <div className="login-popup-condition">
