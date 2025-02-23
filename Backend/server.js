0d3ada4dc0863ccb42e71960f92c1e67039ff2e3
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-02-23 15:26:41 +0530

    Setting up server

--- Backend/server.js
+++ Backend/server.js
@@ -9,3 +9,11 @@ const port = 4000
 // Middleware
 app.use(express.json())
 app.use(cors())
+
+app.get("/",(req,res)=>{
+    res,send("API Working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
