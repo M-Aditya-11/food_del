b4199a0df89e77d316d4f0c1bb3b3ef0c979e337
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-02-23 12:46:20 +0530

    Change in Logic

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -36,11 +36,11 @@ const PlaceOrder = () => {
             </div>
             <div className="cart-total-details">
               <p>Delivery fee</p>
-              <p>${2}</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
             </div>
             <div className="cart-total-details">  
               <b>Total</b>
-              <b>${getTotalCartAmount()+2}</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
           <button >PROCEED TO PAYMENT</button>
