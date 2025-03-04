a806b47ef52eac01e7f48af57f5a76eed5339a86
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-03-04 06:31:42 +0530

    Saving all food item details on MongoDB

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -4,7 +4,23 @@ import fs from 'fs';
 // add food items
 
 const addFood = async(req,res) => {
+    
+    let image_filename = `${req.file.filename}`;
 
+    const food = new foodModel({
+        name:req.body.name,
+        description: req.body.description,
+        price:req.body.price,
+        category: req.body.category,
+        image: image_filename,
+    })
+    try {
+        await food.save();
+        res.json({succes:true,message:"Food Added"})
+    } catch (error) {
+        console.log(error)
+        res.json({succes:false,message:"Error"})
+    }
 }
 
 export {addFood}
