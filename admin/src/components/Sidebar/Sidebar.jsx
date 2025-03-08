89e0f31da6fe24750669ea7d3dde875037276bd7
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-03-08 12:32:26 +0530

    Changed duplicated name

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -16,7 +16,7 @@ const Sidebar = () => {
         </div>
         <div className="sidebar-option">
           <img src={assets.order_icon} alt="" />
-          <p>Add Items</p>
+          <p>Orders</p>
         </div>
       </div>
     </div>
