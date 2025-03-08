eaf3c6e8df49b725154f830c084acc0b68fb548f
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-03-08 23:47:33 +0530

    Added Structure for List Page

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -23,8 +23,28 @@ const List = () => {
   },[])
 
   return (
-    <div>
-        
+    <div className='list add flex-col'>
+      <p>All Food List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index)=>{
+          return(
+            <div key={index} className='list-table-format'>
+              <img src={`${url}/images/`+item.image} />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>{item.price}</p>
+              <p>X</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
