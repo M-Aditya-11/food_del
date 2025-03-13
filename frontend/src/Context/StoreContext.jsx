2fc8493a86d77ca45be9f2c205015f9c02e72a09
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-03-13 08:11:42 +0530

    fetching food list from database

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
 export const StoreContext = createContext(null)
@@ -33,10 +34,19 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    const fetchFoodList = async() =>{
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data);
+    }
+
     useEffect(()=>{
-        if (localStorage.getItem("token")) {
-            setToken(localStorage.getItem("token"));
+        async function loadData(){
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+            }
         }
+        loadData();
     },[])
 
     const contextValue = {
