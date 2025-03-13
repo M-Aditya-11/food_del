e2aeba7c195539a23888dd2cd87836379608061e
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-03-13 07:52:24 +0530

    Debuging the code

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -34,7 +34,7 @@ const LoginPopup = ({ setShowLogin }) => {
     const response = await axios.post(newUrl,data);
 
     if (response.data.success) {
-      setToken(response.data.Token);
+      setToken(response.data.token);
       localStorage.setItem("token",response.data.token)
       setShowLogin(false)
     }else{
