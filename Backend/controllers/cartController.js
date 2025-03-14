8c172ca1ecdd497c20e51a5d1e20b42d426885c6
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-03-14 11:46:43 +0530

    Refactored addToCart function to use findById for user retrieval in cartController

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -4,8 +4,8 @@ import userModel from '../models/userModel.js'
 
 const addToCart = async(req,res) => {
     try {
-        let userDta = await userModel.findOne({_id:req.body.userId});
-        let cartData = await userDta.cartData;
+        let userData = await userModel.findById(req.body.userId);
+        let cartData = await userData.cartData;
         if (!cartData[req.body.itemId]) {
             cartData[req.body.itemId] = 1;
         } else {
