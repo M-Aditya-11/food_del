74e0c4e8a5ba22b8e08664d1d459e635e69e9170
Aditya Mandavkar <adityamandavkar11@outlook.com>
2025-03-14 13:16:05 +0530

    Added orderRouter to server for managing order-related API requests

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from "./routes/foodRoutes.js"
 import userRouter from "./routes/userRoutes.js"
 import 'dotenv/config.js'
 import cartRouter from "./routes/cartRoutes.js"
+import orderRouter from "./routes/orderRoutes.js"
 
 // App Configuration
 
@@ -25,6 +26,7 @@ app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("api/order",orderRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
